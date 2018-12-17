4d6ba1c40a613bc330f67a6a94601352f6967a88
Jiawei Tang <jiaweitang828@gmail.com>
2018-12-17 20:36:17 +0100

    Imported Android Material Design Library

--- app/src/main/java/oth/archaeologicalfieldwork/MainActivity.kt
+++ app/src/main/java/oth/archaeologicalfieldwork/MainActivity.kt
@@ -2,11 +2,14 @@ package oth.archaeologicalfieldwork
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import org.jetbrains.anko.AnkoLogger
+import org.jetbrains.anko.info
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AnkoLogger {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        info("Main Activity started..")
     }
 }
--- app/src/main/java/oth/archaeologicalfieldwork/SplashActivity.kt
+++ app/src/main/java/oth/archaeologicalfieldwork/SplashActivity.kt
@@ -6,17 +6,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 class SplashActivity : AppCompatActivity() {
 
-    //Animation logoAnim;
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
-        //logoAnim = AnimationUtils
-
-        R.anim.logo
-
-
     }
 
 }
