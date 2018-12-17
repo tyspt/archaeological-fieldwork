e835ab1b0b79a1774449170acaca09a6eb22a2c1
Jiawei Tang <jiaweitang828@gmail.com>
2018-12-17 19:49:09 +0100

    Migrate to AndroidX

---
+++ app/src/main/java/oth/archaeologicalfieldwork/SplashActivity.kt
@@ -0,0 +1,22 @@
+package oth.archaeologicalfieldwork
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity;
+
+
+class SplashActivity : AppCompatActivity() {
+
+    //Animation logoAnim;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        //logoAnim = AnimationUtils
+
+        R.anim.logo
+
+
+    }
+
+}
