9e22b92e353a19e863eee209673a7e8d5e4634e2
Jiawei Tang <jiaweitang828@gmail.com>
2018-12-17 20:48:15 +0100

    further work in the Main Activity

--- app/src/main/java/oth/archaeologicalfieldwork/MainActivity.kt
+++ app/src/main/java/oth/archaeologicalfieldwork/MainActivity.kt
@@ -7,9 +7,16 @@ import org.jetbrains.anko.info
 
 class MainActivity : AppCompatActivity(), AnkoLogger {
 
+    //lateinit var sites: SitesStore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //sites = SitesJSONStore(applicationContext)
+        //placemarks = PlacemarkStoreRoom(applicationContext)
+        //placemarks = PlacemarkFireStore(applicationContext)
+
         info("Main Activity started..")
     }
 }
